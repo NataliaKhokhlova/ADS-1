9ff376a0e37b85fcc2476bbf4e88d8bb72d40812
NataliaKhokhlova <76100365+NataliaKhokhlova@users.noreply.github.com>
2021-03-24 09:17:28 +0300

    Update and rename src/alg.cpp to Src/alg.cpp

---
+++ Src/alg.cpp
@@ -0,0 +1,15 @@
+// Copyright 2021 NNTU-CS
+
+int cbinsearch(int arr, int size, int value) {
+  int size = 10; //длина массива
+  int value; //искомое значение
+  int left = 0; int x1; //найденный элемент
+  int arr [n]; 
+  int right = n - 1; 
+while (left <= right){
+     if (arr[n] == x) x1 = n; 
+     if (arr[n] > x) right = n - 1; 
+     else left = n + 1;  
+}
+  return 0; // если ничего не найдено
+}
