834b9e322cdb0be5d759cb61f7cff1cbb0d2e8e4
NataliaKhokhlova <76100365+NataliaKhokhlova@users.noreply.github.com>
2021-06-07 21:06:33 +0300

    Update alg.cpp

--- Src/alg.cpp
+++ Src/alg.cpp
@@ -1,16 +1,22 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int size = 10; // длина массива
-  int value; // искомое значение
-  int left = 0; int x1; // найденный элемент
-  int arr[n];
-  int right = n - 1;
-while (left <= right) {
-  if (arr[n] == x) x1 = n;
-  if (arr[n] > x) right = n -> 1;
-  
-  else left = n + 1;
-}
-  return 0; // если ничего не найденj
-}
+  int r = 0, l = size - 1, k = 0;
+  while (r < l) {
+    int mid = (r + l) / 2;
+    if (arr[mid] < value)
+      r = mid + 1;
+    else
+      l = mid;
+    }
+  if (arr[r] == value) {
+    while (arr[r] == value) {
+      k++;
+      r++;
+      }
+    }
+  if (k != 0)
+    return k;
+  else
+    return 0;
+  }
