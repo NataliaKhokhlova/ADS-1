4ac41d3f9e65fe724be4007582b0e0837116c65d
NataliaKhokhlova <76100365+NataliaKhokhlova@users.noreply.github.com>
2021-06-07 23:52:51 +0300

    Create alg.cpp

---
+++ test/alg.cpp
@@ -0,0 +1,22 @@
+// Copyright 2021 NNTU-CS
+
+int cbinsearch(int *arr, int size, int value) {
+  int r = 0, l = size - 1, k = 0;
+  while (r < l) {
+    int mid = (r + l) / 2;
+    if (arr[mid] < value)
+      r = mid + 1;
+    else
+      l = mid;
+    }
+  if (arr[r] == value) {
+    while (arr[r] == value) {
+      k++;
+      r++;
+      }
+    }
+  if (k != 0)
+    return k;
+  else
+    return 0;
+  }
